9d8f7cdaf7032d765e4c971c2c8115cb1bc373b7
Dimitar Danailov <dimityr.danailov@gmail.com>
2017-01-19 15:02:22 +0200

    Added an example test

---
+++ src/client/app/people/people.controller.spec.js
@@ -0,0 +1,15 @@
+/* jshint -W117, -W030 */
+describe('PeopleController', function() {
+  var controller;
+  // var people = mockData.getMockPeople();
+
+  beforeEach(function() {
+    bard.appModule('app.people');
+    bard.inject('$controller', '$log', '$q', '$rootScope', 'dataservice');
+  });
+
+  it('hello test', function() {
+    expect('hello').to.equal('hello');
+  });
+});
+
